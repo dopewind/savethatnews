ba35a9aa300334c2301abe8a77f12c9470dda289
Lint Action <lint-action@samuelmeuli.com>
2021-05-13 14:42:30 +0000

    🚨 Fix code style issues with Black

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from push.telegram import telegram_bot_sendimage, telegram_bot_sendtext
 from push.twitter import tweet_image
 import requests  # main thing     # get the keys
 from dotenv import load_dotenv
-import os                            # getenv
+import os  # getenv
 import json
 import time
 
@@ -16,9 +16,7 @@ newsapi_key = os.getenv("NEWSAPI_KEY")
 
 # Get news
 
-url = ('https://newsapi.org/v2/top-headlines?'
-       'country=in&'
-       'apiKey=')
+url = "https://newsapi.org/v2/top-headlines?" "country=in&" "apiKey="
 
 url += newsapi_key
 
@@ -31,25 +29,34 @@ except:
 news = json.loads(response.text)
 
 
-for new in news['articles']:
-    author = (str(new['source']['name']))
-    title = (str(new['title']))
-    description = (str(new['description']))
+for new in news["articles"]:
+    author = str(new["source"]["name"])
+    title = str(new["title"])
+    description = str(new["description"])
     if description == None:
         description = ""
-    image_url = (str(new['urlToImage']))
-    full_story = (str(new['url']))
+    image_url = str(new["urlToImage"])
+    full_story = str(new["url"])
 
     # Telggram
-    telegram_messge = "From " + author + "\n \n" + title + "\n \n" + description + "\n \n" + \
-        "[Full Story]({})".format(full_story)
+    telegram_messge = (
+        "From "
+        + author
+        + "\n \n"
+        + title
+        + "\n \n"
+        + description
+        + "\n \n"
+        + "[Full Story]({})".format(full_story)
+    )
 
     telegram_bot_sendimage(image_url, telegram_messge)
 
     # Twitter
 
-    twitter_message = "From " + author + "\n \n" + title + "\n \n" + \
-        "Full story: " + url
+    twitter_message = (
+        "From " + author + "\n \n" + title + "\n \n" + "Full story: " + url
+    )
 
     tweet_image(image_url, twitter_message)
 
--- push/telegram.py
+++ push/telegram.py
@@ -10,9 +10,13 @@ bot_token = os.getenv("BOT_TOKEN")
 
 def telegram_bot_sendtext(bot_message):
 
-    send_text = 'https://api.telegram.org/bot' + bot_token + \
-        '/sendMessage?chat_id=@savethatnews&disable_web_page_preview=false&' + \
-        '&parse_mode=Markdown&text=' + bot_message
+    send_text = (
+        "https://api.telegram.org/bot"
+        + bot_token
+        + "/sendMessage?chat_id=@savethatnews&disable_web_page_preview=false&"
+        + "&parse_mode=Markdown&text="
+        + bot_message
+    )
 
     response = requests.get(send_text)
 
@@ -20,9 +24,15 @@ def telegram_bot_sendtext(bot_message):
 
 
 def telegram_bot_sendimage(url, caption):
-    send_text = 'https://api.telegram.org/bot' + bot_token + \
-        '/sendPhoto?chat_id=@savethatnews&disable_web_page_preview=false&' + \
-        '&photo=' + url + '&parse_mode=Markdown&caption=' + caption
+    send_text = (
+        "https://api.telegram.org/bot"
+        + bot_token
+        + "/sendPhoto?chat_id=@savethatnews&disable_web_page_preview=false&"
+        + "&photo="
+        + url
+        + "&parse_mode=Markdown&caption="
+        + caption
+    )
 
     response = requests.get(send_text)
 
