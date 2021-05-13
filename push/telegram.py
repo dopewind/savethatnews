70c502f07e65a36137bc3f24f22f6b8aef386e98
Lint Action <lint-action@samuelmeuli.com>
2021-05-13 14:49:02 +0000

    🚨 Fix code style issues with Black

--- push/telegram.py
+++ push/telegram.py
@@ -9,9 +9,13 @@ bot_token = os.getenv("BOT_TOKEN")
 
 def telegram_bot_sendtext(bot_message):
 
-    send_text = 'https://api.telegram.org/bot' + bot_token + \
-        '/sendMessage?chat_id=@savethatnews&disable_web_page_preview=false&' +\
-        '&parse_mode=Markdown&text=' + bot_message
+    send_text = (
+        "https://api.telegram.org/bot"
+        + bot_token
+        + "/sendMessage?chat_id=@savethatnews&disable_web_page_preview=false&"
+        + "&parse_mode=Markdown&text="
+        + bot_message
+    )
 
     response = requests.get(send_text)
 
--- tools/img.py
+++ tools/img.py
@@ -6,8 +6,11 @@ def img_download(link, filename):
     # Adding information about user agent
     opener = urllib.request.build_opener()
     opener.addheaders = [
-        ('User-Agent',
-         'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        (
+            "User-Agent",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36",
+        )
+    ]
     urllib.request.install_opener(opener)
 
     image_url = link
