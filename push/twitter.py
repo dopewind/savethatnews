6ace076d988c7ac941b69939e3334adc750c316d
Karthik Vinayan <omendoesstuff@gmail.com>
2021-05-13 19:29:00 +0530

    :sparkles: Tweets images with caption

--- push/twitter.py
+++ push/twitter.py
@@ -1,6 +1,7 @@
 import tweepy
 from dotenv import load_dotenv
 import os
+import requests
 
 # personal details
 load_dotenv()
@@ -22,3 +23,29 @@ def tweet_this(message):
 
     # update the status
     api.update_status(status=message)
+
+
+def twitter_api():
+    # authentication of consumer key and secret
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+    # authentication of access token and secret
+
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    return api
+
+
+def tweet_image(url, message):
+    api = twitter_api()
+    filename = url.split("/")[-1]
+    request = requests.get(url, stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as image:
+            for chunk in request:
+                image.write(chunk)
+
+        api.update_with_media(filename, status=message)
+        os.remove(filename)
+    else:
+        print("Unable to download image")
